d674a18b71986a8a139ed1247da222da5705eae3
jassu1 <93320373+jassu1@users.noreply.github.com>
2022-08-24 16:24:32 +0930

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>C-77</title>
+    </head>
+    <body>
+        <h1>
+         console
+        </h1>
+        <p>
+            Visual Studio Code is an easy way to use javascript. To set it up you need to download it then create a file on your desktop then open the file and start coding!
+        </p>
+        <p>
+            The console screen is basically the display of what the code will produce
+        </p>
+           
+ </body>        
+ </html> 
